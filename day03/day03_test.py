d93422966aa68968697be9c27a0debfea5d36252
George Murga <george.murga+github@gmail.com>
2023-07-24 19:09:50 +0300

    Trying something fancy with "templates".

--- day03/day03_test.py
+++ day03/day03_test.py
@@ -1,15 +1,31 @@
-# from day03 import # type: ignore
+from importlib.machinery import ModuleSpec
+from pathlib import Path
+import sys
+from importlib import util
+from types import ModuleType
+
+full_path = Path(__file__)
+file_path: Path = full_path.parent / full_path.name.replace("_test", "")
+module_name: str = file_path.stem
+
+spec: ModuleSpec | None = util.spec_from_file_location(module_name, file_path)
+module: ModuleType = util.module_from_spec(spec)  # type: ignore
+spec.loader.exec_module(module)  # type: ignore
+sys.modules["solutions"] = module
+
+from solutions import solve_part1, solve_part2  # type: ignore # noqa: E402
 
 
 INPUT_S: str = """
 """
 
 
+def banner(msg: str) -> str:
+    return f"\n{'=' * len(msg)}{msg}{'-' * len(msg)}"
+
+
 def main() -> None:
-    msg: str = "TESTING Part1"
-    print(f"\n{'=' * len(msg)}\n")
-    print(msg)
-    print(f"\n{'-' * len(msg)}\n")
+    pass
 
 
 if __name__ == "__main__":
