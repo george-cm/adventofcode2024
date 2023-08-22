59d118769d924da1e8a8771e32d6aed6223eb08f
George Murga <george.murga@gmail.com>
2023-08-22 20:25:40 +0300

    Iclude problem_dayN.txt file when using create_new_day

--- src/adventofcode2022/common.py
+++ src/adventofcode2022/common.py
@@ -12,11 +12,11 @@ def load_input(inputfile: Path) -> str:
 def create_day(day: int) -> None:
     base_path = Path(__file__).parent
 
-    day_file = Path(base_path / "template/day${day}.py")
+    day_file = base_path / "template/day${day}.py"
     new_day_file_name_template = Template(day_file.name)
     day_str = f"{day:02d}"
     new_day_file_name = f"{new_day_file_name_template.substitute(day=day_str)}"
-    new_day_fld = Path(base_path / f"day{day_str}")
+    new_day_fld = base_path / f"day{day_str}"
 
     if new_day_fld.exists():
         raise OSError("Folder exists: '%s'" % new_day_fld)
@@ -26,11 +26,11 @@ def create_day(day: int) -> None:
     with day_file.open("r", encoding="utf-8") as f:
         new_day_file_content = f.read()
 
-    new_day_file = Path(new_day_fld / new_day_file_name)
+    new_day_file = new_day_fld / new_day_file_name
     with new_day_file.open("w", encoding="utf-8") as f:
         f.write(new_day_file_content)
 
-    day_test_file = Path(base_path / "template/day${day}_test.py")
+    day_test_file = base_path / "template/day${day}_test.py"
     new_day_test_file_name_template = Template(day_test_file.name)
     new_day_test_file_name = (
         f"{new_day_test_file_name_template.substitute(day=day_str)}"
@@ -39,9 +39,12 @@ def create_day(day: int) -> None:
     with day_test_file.open("r", encoding="utf-8") as f:
         new_day_test_file_content = f.read()
 
-    new_day_test_file = Path(new_day_fld / new_day_test_file_name)
+    new_day_test_file = new_day_fld / new_day_test_file_name
     with new_day_test_file.open("w", encoding="utf-8") as f:
         f.write(new_day_test_file_content)
+
+    new_day_problem_statement_file = new_day_fld / f"problem_day{day_str}.txt"
+    new_day_problem_statement_file.write_text("")
 
 
 def main():
