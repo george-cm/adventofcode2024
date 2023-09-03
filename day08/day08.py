fb9765b1ed1c9d68931592fe21f45c91b9c278a4
George Murga <george.murga@gmail.com>
2023-09-03 15:11:43 +0300

    Day08 solution to part 2.

--- day08/day08.py
+++ day08/day08.py
@@ -21,11 +21,6 @@ def get_col(col: int, grid: list[list[int]]) -> list[int]:
 def is_tree_visible(tree: tuple[int, int], grid: list[list[int]]) -> bool:
     row: list[int] = get_row(tree[0], grid)
     col: list[int] = get_col(tree[1], grid)
-    # pprint(grid)
-    # print(f"tree value {grid[tree[0]][tree[1]]}")
-    # print(f"{tree=}")
-    # print(f"{row=}")
-    # print(f"{col=}")
     tree_value: int = grid[tree[0]][tree[1]]
     return (
         tree_value > max(row[: tree[1]])
@@ -36,28 +31,66 @@ def is_tree_visible(tree: tuple[int, int], grid: list[list[int]]) -> bool:
 
 
 def calculate_perimeter(grid: list[list[int]]) -> int:
-    return 4 * len(grid) - 4
+    # return 4 * len(grid) - 4
+    return (2 * len(grid)) + (2 * (len(grid[0]) - 2))
+
+
+def tree_seenic_score(tree: tuple[int, int], grid: list[list[int]]):
+    row: list[int] = get_row(tree[0], grid)
+    col: list[int] = get_col(tree[1], grid)
+    tree_value: int = grid[tree[0]][tree[1]]
+
+    # look left
+    left_visibility = 0
+    for other_tree in reversed(row[: tree[1]]):
+        left_visibility += 1
+        if other_tree >= tree_value:
+            break
+
+    # look right
+    right_visibility = 0
+    for other_tree in row[tree[1] + 1 :]:
+        right_visibility += 1
+        if other_tree >= tree_value:
+            break
+
+    # look up
+    up_visibility = 0
+    for other_tree in reversed(col[: tree[0]]):
+        up_visibility += 1
+        if other_tree >= tree_value:
+            break
+
+    # look down
+    down_visibility = 0
+    for other_tree in col[tree[0] + 1 :]:
+        down_visibility += 1
+        if other_tree >= tree_value:
+            break
+
+    return left_visibility * right_visibility * up_visibility * down_visibility
 
 
 def solve_part1(inputs: str) -> int:
     # write code here, update rusult
     grid: list[list[int]] = create_grid(inputs)
     count_visible_trees: int = calculate_perimeter(grid)
-    for row in range(1, len(grid) - 1):
-        for col in range(1, len(grid) - 1):
-            # print(
-            #     f"tree {row},{col} is visible? {is_tree_visible((row, col), grid)}"
-            # )
-            if is_tree_visible((row, col), grid):
-                count_visible_trees += 1
+    row_range = range(1, len(grid[0]) - 1)
+    col_range = range(1, len(grid) - 1)
+    count_visible_trees += sum(
+        is_tree_visible((row, col), grid) for row in row_range for col in col_range
+    )
     return count_visible_trees
 
 
 def solve_part2(inputs: str) -> int:
-    result: int = 0
     # write code here, update rusult
-
-    return result
+    grid: list[list[int]] = create_grid(inputs)
+    row_range = range(len(grid[0]))
+    col_range = range(len(grid))
+    return max(
+        tree_seenic_score((row, col), grid) for row in row_range for col in col_range
+    )
 
 
 def main() -> None:
--- day08/day08_test.py
+++ day08/day08_test.py
@@ -1,12 +1,20 @@
 import pytest
 
-from day08 import solve_part1, solve_part2  # type: ignore  # noqa: E402
+from day08 import tree_seenic_score  # type: ignore  # noqa: E402
+from day08 import create_grid, solve_part1, solve_part2  # type: ignore  # noqa: E402
 
 INPUT_S: str = """30373
 25512
 65332
 33549
 35390"""
+
+
+def test_tree_scenic_score():
+    tree = (1, 2)
+    grid = create_grid(INPUT_S)
+    scenic_score = tree_seenic_score(tree, grid)
+    assert scenic_score == 4
 
 
 def test_solve_part1() -> None:
