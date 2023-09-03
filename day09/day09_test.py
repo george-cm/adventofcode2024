2dc987c3207617d629bc544855a256738dd5499d
George Murga <george.murga@gmail.com>
2023-09-03 15:33:46 +0300

    Prep test for day09 part 1.

--- day09/day09_test.py
+++ day09/day09_test.py
@@ -1,14 +1,19 @@
-import sys
-
 import pytest
-from day09 import solve_part1, solve_part2  # type: ignore  # noqa: E402
 
-INPUT_S: str = """"""
+from day09 import solve_part1, solve_part2  # type: ignore  # noqa: E402 F401
+
+INPUT_S: str = """R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2"""
 
 
 def test_solve_part1() -> None:
-    # assert solve_part1(INPUT_S) ==
-    ...
+    assert solve_part1(INPUT_S) == 13
 
 
 def test_solve_part2() -> None:
