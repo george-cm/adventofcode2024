b7bbf7c42de55e6851ba47118c349fc68364aa25
George Murga <george.murga@gmail.com>
2023-09-22 20:18:04 +0300

    day09 part 2 solved.

--- day09/day09.py
+++ day09/day09.py
@@ -31,6 +31,9 @@ class Position:
     def __gt__(self, other: "Position") -> bool:
         return any((self.row > other.row, self.col > other.col))
 
+    def swap(self):
+        return Position(self.col, self.row)
+
 
 class Grid:
     def __init__(
@@ -38,17 +41,21 @@ class Grid:
         rows: int,
         cols: int,
         head_pos: Position,
-        tail_pos: Position,
+        segments: int = 2,
         visualize: bool = False,
+        padding_rows: int = 0,
+        padding_cols: int = 0,
     ) -> None:
         self.rows: int = rows
         self.cols: int = cols
         self.min_row = self.max_row = 5
         self.min_col = self.max_col = 5
         self.head_pos: Position = head_pos
-        self.tail_pos: Position = tail_pos
-        self.tail_visited_positions: list[Position] = [self.tail_pos]
+        self.segments: list[Position] = [head_pos for _ in range(segments)]
+        self.tail_visited_positions: list[Position] = [self.segments[-1]]
         self.visualize = visualize
+        self.padding_rows = padding_rows
+        self.padding_cols = padding_cols
 
     def _parse_move(self, move_instruction: str) -> tuple[Position, int]:
         split_move_instruction: list[str] = move_instruction.split(" ")
@@ -72,7 +79,7 @@ class Grid:
                 raise NotImplementedError(f"{move_instruction=} not implemented.")
         return direction, steps
 
-    def move_head(self, move_instruction: str) -> None:
+    def move_segments(self, move_instruction: str) -> None:
         if self.visualize:
             print(f"========= Moving {move_instruction} =========")
             print("Starting position:")
@@ -84,36 +91,47 @@ class Grid:
         steps: int
         direction, steps = self._parse_move(move_instruction)
         for _ in range(steps):
-            self.head_pos += direction
+            # self.head_pos += direction
+            self.segments[0] += direction
             self.update_grid_size()
-            head_tail_delta = self.head_pos - self.tail_pos
-            if any(
-                (
-                    abs(head_tail_delta) == Position(1, 2),
-                    abs(head_tail_delta) == Position(2, 1),
-                    abs(head_tail_delta) == Position(0, 2),
-                    abs(head_tail_delta) == Position(2, 0),
-                )
-            ):
-                self.tail_pos += head_tail_delta - direction
-                self.tail_visited_positions.append(self.tail_pos)
+            # delta: Position = self.segments[0] - self.segments[1]
+            # to_move: Position = delta - direction
+            # move all the remaining segments
+            for i in range(1, len(self.segments)):
+                segments_delta = self.segments[i - 1] - self.segments[i]
+                if abs(segments_delta) > Position(1, 1):
+                    # self.segments[i] += segments_delta - direction
+                    self.segments[i] += self._clamp(segments_delta)
+                    # self.segments[i] += to_move
+                    if i == len(self.segments) - 1:
+                        self.tail_visited_positions.append(self.segments[i])
             if self.visualize:
                 print(self)
                 print()
+
+    def _clamp(
+        self, pos: Position, min_value: int = -1, max_value: int = 1
+    ) -> Position:
+        row: int = max(min(pos.row, max_value), min_value)
+        col: int = max(min(pos.col, max_value), min_value)
+        return Position(row, col)
 
     def unique_tail_positions(self):
         return set(self.tail_visited_positions)
 
     def update_grid_size(self) -> None:
-        self.max_row = max(self.head_pos.row, self.max_row)
-        self.min_row = min(self.head_pos.row, self.min_row)
-        self.max_col = max(self.head_pos.col, self.max_col)
-        self.min_col = min(self.head_pos.col, self.max_col)
+        self.max_row = max(self.segments[0].row, self.max_row)
+        self.min_row = min(self.segments[0].row, self.min_row)
+        self.max_col = max(self.segments[0].col, self.max_col)
+        self.min_col = min(self.segments[0].col, self.min_col)
 
     def visited_tail_positions(self) -> str:
         grid: list[list[str]] = []
         max_pos = self._normalize_pos(Position(self.max_row, self.max_col))
-        grid = [["."] * (max_pos.col + 1) for _ in range(max_pos.row + 1)]
+        grid = [
+            ["."] * (max_pos.col + self.padding_cols + 1)
+            for _ in range(max_pos.row + self.padding_rows + 1)
+        ]
 
         for pos in self.unique_tail_positions():
             norm_pos = self._normalize_pos(pos)
@@ -126,16 +144,20 @@ class Grid:
         return pos + abs(Position(min(self.min_row, 0), min(self.min_col, 0)))
 
     def __str__(self) -> str:
+        # print(f"{self.max_row=}, {self.max_col=}")
         grid: list[list[str]] = []
         max_pos = self._normalize_pos(Position(self.max_row, self.max_col))
-        grid = [["."] * (max_pos.col + 1) for _ in range(max_pos.row + 1)]
-        # prev_head_pos: Position = self.head_pos
+        grid = [
+            ["."] * (max_pos.col + self.padding_cols + 1)
+            for _ in range(max_pos.row + self.padding_rows + 1)
+        ]
         grid[0][0] = "s"
-        head_pos = self._normalize_pos(self.head_pos)
-        tail_pos = self._normalize_pos(self.tail_pos)
-        grid[head_pos.row][head_pos.col] = "H"
-        if tail_pos != head_pos:
-            grid[tail_pos.row][tail_pos.col] = "T"
+        for i, pos in enumerate(reversed(self.segments)):
+            norm_pos = self._normalize_pos(pos)
+            # print(f"segment: {len(self.segments) - 1 - i} {norm_pos=}")
+            grid[norm_pos.row][norm_pos.col] = str(len(self.segments) - 1 - i)
+        norm_head_pos = self._normalize_pos(self.segments[0])
+        grid[norm_head_pos.row][norm_head_pos.col] = "H"
         return "\n".join([" ".join(row) for row in reversed(grid)])
 
 
@@ -147,20 +169,30 @@ def solve_part1(inputs: str, visualize: bool = False) -> int:
     current_pos: Position = Position(row, col)
     moves: list[str] = inputs.splitlines()
     head_pos: Position = current_pos
-    tail_pos: Position = current_pos
-    grid = Grid(0, 0, head_pos, tail_pos, visualize=visualize)
+    # tail_pos: Position = current_pos
+    grid = Grid(0, 0, head_pos, 2, visualize=visualize)
     for move in moves:
-        grid.move_head(move)
+        grid.move_segments(move)
     if visualize:
         print(grid.visited_tail_positions())
     return len(grid.unique_tail_positions())
 
 
-def solve_part2(inputs: str) -> int:
-    result: int = 0
+def solve_part2(inputs: str, visualize: bool = False) -> int:
+    # result: int = 0
     # write code here, update rusult
-
-    return result
+    row: int = 0
+    col: int = 0
+    current_pos: Position = Position(row, col)
+    moves: list[str] = inputs.splitlines()
+    head_pos: Position = current_pos
+    # tail_pos: Position = current_pos
+    grid = Grid(0, 0, head_pos, 10, visualize=visualize)
+    for move in moves:
+        grid.move_segments(move)
+    if visualize:
+        print(grid.visited_tail_positions())
+    return len(grid.unique_tail_positions())
 
 
 def main() -> None:
--- day09/day09_test.py
+++ day09/day09_test.py
@@ -13,11 +13,23 @@ R 2"""
 
 
 def test_solve_part1() -> None:
-    assert solve_part1(INPUT_S, True) == 13
+    assert solve_part1(INPUT_S, visualize=False) == 13
 
 
-def test_solve_part2() -> None:
-    assert solve_part2(INPUT_S) == 1
+def test_solve_part2_short() -> None:
+    assert solve_part2(INPUT_S, visualize=True) == 1
+
+
+def test_solve_part2_long() -> None:
+    inputs = """R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20"""
+    assert solve_part2(inputs, visualize=True) == 36
 
 
 if __name__ == "__main__":
