6e9c5d8aa4b9b1d24241ea386ee2b3aa01b414d6
George Murga <george.murga+github@gmail.com>
2025-01-02 22:19:44 +0200

    Day07 part1 and part2 - complete

--- day07/day07.py
+++ day07/day07.py
@@ -1,3 +1,4 @@
+from functools import cache
 from pathlib import Path
 
 from adventofcode.common import load_input
@@ -28,51 +29,68 @@ def parse_input(input_str: str) -> list[dict[int, list[int]]]:
     ]
 
 
-def combine_operators(num_operators: int, operators: list[str]) -> list[list[str]]:
-
-    def create_combination(
-            previous: list[str], num_operators: int, operators: list[str]) -> list[str]:
-        if num_operators == 0:
-            return previous
-        for operator in operators:
-            previous.append(operator)
-            previous.extend(create_combination(previous, num_operators-1, operators))
-        return previous
-
-    combinations: list[list[str]] = []
-    previous: list[str] = []
+@cache
+def create_combination(
+    num_operators: int, operators: tuple[str, ...]
+) -> list[list[str]]:
+    if num_operators == 0:
+        return []
+    accumulator = []
     for operator in operators:
-        combinations.append(create_combination(previous, num_operators, operators))
-        previous = []
-    return combinations
+        next_combinations = create_combination(num_operators - 1, operators)
+        if next_combinations:
+            for combination in next_combinations:
+                accumulator.append([operator] + combination)
+        else:
+            accumulator.append([operator])
+    return accumulator
 
 
 def is_valid_calibration(
-    calibration: dict[int, list[int]], operators: list[str]
+    calibration: dict[int, list[int]], operators: tuple[str, ...]
 ) -> bool:
     test_value = list(calibration.keys())[0]
     operands = list(calibration.values())[0]
-    # print(test_value)
-    # print(operands)
+    operator_combinations = create_combination(len(operands) - 1, operators)
+    for operator_combination in operator_combinations:
+        acc = operands[0]
+        for i, operator in enumerate(operator_combination):
+            match operator:
+                case "+":
+                    acc += operands[i + 1]
+                case "*":
+                    acc *= operands[i + 1]
+                case "||":
+                    acc = int(str(acc) + str(operands[i + 1]))
+            if acc > test_value:
+                # print(
+                #     f"{test_value=} | {acc=} | {operands=} | {operator_combination=}"
+                # )
+                break
+        if acc == test_value:
+            return True
+    return False
 
 
 def solve_part1(inputs: str) -> int:
-    operators = ["+", "*"]
+    operators = ("+", "*")
     calibrations = parse_input(inputs)
     valid_test_values: list[int] = []
     for calibration in calibrations:
         if is_valid_calibration(calibration, operators):
             valid_test_values.append(list(calibration.keys())[0])
-    result: int = 0
-    # write code here, update rusult
-
+    result: int = sum(valid_test_values)
     return result
 
 
 def solve_part2(inputs: str) -> int:
-    result: int = 0
-    # write code here, update rusult
-
+    operators = ("+", "*", "||")
+    calibrations = parse_input(inputs)
+    valid_test_values: list[int] = []
+    for calibration in calibrations:
+        if is_valid_calibration(calibration, operators):
+            valid_test_values.append(list(calibration.keys())[0])
+    result: int = sum(valid_test_values)
     return result
 
 
--- day07/day07_test.py
+++ day07/day07_test.py
@@ -4,10 +4,11 @@ import pytest
 
 from adventofcode.common import load_input
 from day07 import (  # type: ignore  # noqa: E402
+    create_combination,
+    is_valid_calibration,
     parse_input,
     solve_part1,
     solve_part2,
-    combine_operators,
 )
 
 INPUT_S: str = load_input(Path(__file__).parent / "example.txt")
@@ -28,20 +29,49 @@ def test_parse_input() -> None:
     ]
 
 
-def test_combine_operators():
-    operators = ['+', '*']
-    expected = [['+', '+'], ['+', '*'], ['*', '+'], ['*', '*']]
-    result = combine_operators(2, operators)
-    print(result)
+def test_create_combination_1():
+    operators = ("+", "*")
+    expected = [["+"], ["*"]]
+    result = create_combination(1, operators)
     assert result == expected
 
 
+def test_create_combination_2():
+    operators = ("+", "*")
+    expected = [["+", "+"], ["+", "*"], ["*", "+"], ["*", "*"]]
+    result = create_combination(2, operators)
+    assert result == expected
+
+
+def test_create_combination_3():
+    operators = ("+", "*")
+    expected = [
+        ["+", "+", "+"],
+        ["+", "+", "*"],
+        ["+", "*", "+"],
+        ["+", "*", "*"],
+        ["*", "+", "+"],
+        ["*", "+", "*"],
+        ["*", "*", "+"],
+        ["*", "*", "*"],
+    ]
+    result = create_combination(3, operators)
+    assert result == expected
+
+
+def test_is_valid_calibration():
+    calibration = {7290: [6, 8, 6, 15]}
+    operators = ("+", "*", "||")
+    result = is_valid_calibration(calibration, operators)
+    assert result == True
+
+
 def test_solve_part1() -> None:
-    assert solve_part1(INPUT_S) == -1
+    assert solve_part1(INPUT_S) == 3749
 
 
 def test_solve_part2() -> None:
-    assert solve_part2(INPUT_S) == -1
+    assert solve_part2(INPUT_S) == 11387
 
 
 if __name__ == "__main__":
